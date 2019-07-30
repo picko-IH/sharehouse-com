3c3184aec3b8e2c81b0022a417eddeb7ca9bd9cc
picko-IH <picko.ih@gmail.com>
2019-07-30 13:38:23 +0000

    create db test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+if House.count == 0
+  10.times do
+    House.create(
+      name: "クランテラス 品川",
+      address: "品川区大井4丁目",
+      access: "JR京浜東北線 大井町駅 徒歩9分",
+      description: "入居者様にとっての暮らしやすさ、居心地の良さとは何か、を追求しクランテラス品川を設計いたしました。お部屋の天井高は3ｍを確保し、どのお部屋も明るく新生活をスタートするにはピッタリのお家となっております。当社ならではの空間づくりをぜひご覧下さい！",
+      gender_type: "male"
+    )
+  end
+end

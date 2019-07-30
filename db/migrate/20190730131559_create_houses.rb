e5eab87003c6b2d82341cf95b2eece819b78d9bb
picko-IH <picko.ih@gmail.com>
2019-07-30 13:19:56 +0000

    create house db table

---
+++ db/migrate/20190730131559_create_houses.rb
@@ -0,0 +1,13 @@
+class CreateHouses < ActiveRecord::Migration[5.0]
+  def change
+    create_table :houses do |t|
+      t.string :name
+      t.string :address
+      t.string :access
+      t.text :description
+      t.string :gender_type
+
+      t.timestamps
+    end
+  end
+end

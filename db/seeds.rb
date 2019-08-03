f33538d2d7ad67d42fae29a19017787bfe06a386
picko-IH <picko.ih@gmail.com>
2019-08-03 01:16:27 +0000

    テスト完了

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-if Houses.count == 0
+if House.count == 0
   10.times do
-    Houses.create(
+    House.create(
       name: "ハウス名",
       address: "住所",
       access: "アクセス",

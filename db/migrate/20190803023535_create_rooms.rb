b1e653942e714e6d81ab00cee4a5db72ec6a6619
picko-IH <picko.ih@gmail.com>
2019-08-10 01:42:28 +0000

    詳細ページテスト完了

--- db/migrate/20190803023535_create_rooms.rb
+++ db/migrate/20190803023535_create_rooms.rb
@@ -1,15 +1,5 @@
 class CreateRooms < ActiveRecord::Migration[5.0]
   def change
-    create_table :houses do |t|
-      t.string :name
-      t.string :address
-      t.string :access
-      t.text :description
-      t.string :gender_type
-
-      t.timestamps
-    end
-    
     create_table :rooms do |t|
       t.integer :house_id
       t.string :name
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-#  if House.count == 0
-#    10.times do
-#      House.create(
-#        name: "ハウス名",
-#        address: "住所",
-#        access: "アクセス",
-#        description: "説明",
-#        gender_type: "男女"
-#     )
-#    end
-#  end
-
-if room.count == 0
-  10.times do
-    room.create(
-      house_id: "2",
-      name: "部屋名",
-    )
+  if House.count == 0
+    10.times do
+      house = House.create(
+        name: "ハウス名",
+        address: "住所",
+        access: "アクセス",
+        description: "説明",
+        gender_type: "男女"
+      )
+      10.times do
+        Room.create(
+          house_id: house.id,
+          name: "部屋名",
+        )
+      end
+    end
   end
-end

3e354832650e7c139c5dd806f714212eccfbc154
picko-IH <picko.ih@gmail.com>
2019-08-31 19:58:10 +0000

    検索機能完了、ransack検討中

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -3,6 +3,7 @@ class HousesController < ApplicationController
     rooms = Room.all
     rooms = rooms.where('rent >= ?', params[:min_rent]) if params[:min_rent].present?
     rooms = rooms.where('rent <= ?', params[:max_rent]) if params[:max_rent].present?
+    rooms = rooms.where('gender_type <= ?', params[:gender_type]) if params[:gender_type].present?
     @houses = House.where(id: rooms.pluck(:house_id).uniq)
   end
   

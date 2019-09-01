477cafa3ed9c0f649b4bed89188d6950f0bf094b
picko-IH <picko.ih@gmail.com>
2019-09-01 03:13:54 +0000

    新規物件登録機能追加

--- app/controllers/admin/houses_controller.rb
+++ app/controllers/admin/houses_controller.rb
@@ -3,4 +3,24 @@ class Admin::HousesController < ApplicationController
   def index
     @houses = House.all
   end
+  
+  def new
+    @house = House.new
+  end
+  
+  def create
+    @house = House.new(house_params)
+    if @house.save
+      redirect_to admin_houses_path, notice: "物件登録しました。"
+    else
+      render 'new'
+    end
+  end
+
+  private
+    # Rails4からStrongParamaterと呼ばれる機能が追加されました。
+    # セキュリティのため、permitメソッドで許可したパラメータ名しか取得できません。
+    def house_params
+      params.require(:house).permit(:name, :address, :access, :description, :gender_type)
+    end
 end
